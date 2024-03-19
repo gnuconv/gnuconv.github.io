69501f7995ab874617402413da2542cb6dd226ce
gnuconv <gnuconv@gmail.com>
2024-03-19 09:22:00 -0400

    Make RBC converter work

--- src/processors.ts
+++ src/processors.ts
@@ -1,6 +1,11 @@
 import { Transaction } from "./InputFile";
 import Papa from "papaparse";
 
+const mdyToYmd = (date: string) => {
+  const parts = date.split("/");
+  return `${parts[2]}-${parts[0]}-${parts[1]}`;
+};
+
 export const processors: Record<string, (str: string) => Transaction[]> = {
   TD: (str: string) => {
     const rows: string[][] = Papa.parse(str).data.filter(
@@ -29,6 +34,21 @@ export const processors: Record<string, (str: string) => Transaction[]> = {
     });
   },
   RBC: (str: string) => {
-    return [];
+    const formatDesc = (desc1: string, desc2: string) => {
+      const x = [];
+      if (desc1) x.push(desc1.trim());
+      if (desc2) x.push(desc2.trim());
+      return x.join("/");
+    };
+    const rows: string[][] = Papa.parse(str)
+      .data.slice(1)
+      .filter((c: any) => c.length > 1) as unknown as string[][];
+    return rows.map((r) => ({
+      account: r[0],
+      date: mdyToYmd(r[2]),
+      description: formatDesc(r[4], r[5]),
+      destination: "Expenses:UNKNOWN",
+      amount: parseFloat(r[6]),
+    }));
   },
 };
